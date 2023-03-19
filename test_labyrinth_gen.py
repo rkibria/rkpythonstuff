ab225f9de62bc03b68c4ba8cc97160dfb74d3039
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2023-03-19 13:23:36 +0000

    Labyrinth generator (#1)

---
+++ test_labyrinth_gen.py
@@ -0,0 +1,26 @@
+from labyrinth_gen import make_labyrinth
+
+def test_single_cell():
+    labyrinth = make_labyrinth(1, 1, 0)
+    assert(labyrinth["size"] == (1,1))
+    assert(labyrinth["start"] == (0,0))
+    assert(labyrinth["ends"] == [])
+    assert(labyrinth["cells"] == [[[True, True, True, True]]]) # Walls: NSWE, True=closed
+
+def test_two_columns_by_one_row():
+    labyrinth = make_labyrinth(1, 2, 0)
+    assert(labyrinth["size"] == (1, 2))
+    assert(labyrinth["start"] in [(0, 0), (0, 1)]) # row/column coordinates!
+    assert(labyrinth["ends"] == [(0, 0), (0, 1)])
+    # open east wall <-> open west wall
+    assert(labyrinth["cells"] == [[[True, True, True, False], [True, True, False, True]]])
+
+def test_one_column_by_two_rows():
+    labyrinth = make_labyrinth(2, 1, 0)
+    assert(labyrinth["size"] == (2, 1))
+    assert(labyrinth["start"] in [(0, 0), (1, 0)])
+    assert(labyrinth["ends"] == [(0, 0), (1, 0)])
+    # open south wall
+    # |
+    # open north wall
+    assert(labyrinth["cells"] == [[[True, False, True, True]], [[False, True, True, True]]])
